5772a3d41fb33e2f30a788c4fa4650d289bef780
唐海岚 <gdx_lwx@qq.com>
2021-02-05 00:32:23 +0800

    antd引入测试

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import {Button,message} from 'antd'
 /*
  应用的根组件
  */
 export default class App extends Component{
+    handleClick = () =>{
+        message.success("success")
+    }
     render() {
-        return <div>App</div>
+        return <Button type="primary" onClick={this.handleClick}>测试antd</Button>
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'antd/dist/antd.css'
 import App from "./App";
 
 //将App组件标签渲染到index页面的div上
