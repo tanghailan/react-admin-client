0be9c3ec375c8b9b827594428ca7e88bfc9b56bd
唐海岚 <gdx_lwx@qq.com>
2021-02-05 23:07:40 +0800

    admin login 组件新增

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
-import {Button,message} from 'antd'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 /*
  应用的根组件
  */
 export default class App extends Component{
-    handleClick = () =>{
-        message.success("success")
-    }
     render() {
-        return <Button type="primary" onClick={this.handleClick}>测试antd</Button>
+        return <BrowserRouter>
+            <Switch>
+                {/*只匹配其中一个*/}
+                <Route path='/login' component={Login}></Route>
+                <Route path='/' component={Admin}></Route>
+            </Switch>
+        </BrowserRouter>
     }
 }
