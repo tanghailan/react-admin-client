eda2520340575cb3c446e9167e20598214c6a926
唐海岚 <gdx_lwx@qq.com>
2021-02-06 17:32:21 +0800

    提交login组件页面编写

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,18 @@
 import React,{Component} from 'react'
-
-
+import './login.less'
+import logo from './images/logo192.png'
+/*
+ 登录的路由组件
+ */
 export default class Login extends  Component{
     render() {
-        return <div>login</div>
+        return<div className="login">
+            <header className="login-header">
+                <img src={logo} alt="logo"/>
+            </header>
+            <section className="login-content">
+
+            </section>
+        </div>
     }
 }
